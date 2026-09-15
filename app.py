8d203d67d4f547e20330833e71fa26e0884bc960
cocacolac45 <nhi.lht.456@gmail.com>
2026-09-16 05:14:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import joblib
 
+# =========================
+# LOAD MODEL
+# =========================
+
 model = joblib.load("svm_iris_model.pkl")
 encoder = joblib.load("iris_encoder.pkl")
+
+# =========================
+# FASTAPI
+# =========================
 
 app = FastAPI(
     title="Iris Classification API",
@@ -12,11 +21,24 @@ app = FastAPI(
     version="1.0.0",
 )
 
+# Cho phép website truy cập ảnh trong folder photo
+app.mount("/photo", StaticFiles(directory="photo"), name="photo")
+
+
+# =========================
+# INPUT DATA
+# =========================
+
 class IrisInput(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
+
+
+# =========================
+# CLASS
+# =========================
 
 species = {
     0: "setosa",
@@ -24,17 +46,48 @@ species = {
     2: "virginica",
 }
 
+
+# =========================
+# TRANG CHỦ
+# =========================
+
 @app.get("/", response_class=HTMLResponse)
 def home():
+
     with open("index.html", "r", encoding="utf-8") as f:
         return f.read()
 
+
+# =========================
+# TRANG DỰ ĐOÁN
+# =========================
+
+@app.get("/predict-page", response_class=HTMLResponse)
+def predict_page():
+
+    with open("predict.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+# =========================
+# HEALTH CHECK
+# =========================
+
 @app.get("/health")
 def health():
-    return {"status": "healthy"}
+
+    return {
+        "status": "healthy"
+    }
+
+
+# =========================
+# PREDICT API
+# =========================
 
 @app.post("/predict")
 def predict(data: IrisInput):
+
     features = [[
         data.sepal_length,
         data.sepal_width,
