c74bb809f5634fab6a768163f90aa30171c9da71
cocacolac45 <nhi.lht.456@gmail.com>
2026-09-17 08:30:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,11 +39,6 @@ class IrisInput(BaseModel):
 # TRANG CHỦ
 # =========================
 
-@app.get("/", response_class=HTMLResponse)
-def home():
-
-    with open("index.html", "r", encoding="utf-8") as f:
-        return f.read()
 
 # =========================
 # TRANG DỰ ĐOÁN
